e17e4c47ee473e669bbacf41cffc1412f6b08e70
anisyan <anisimov.alexander.s@gmail.com>
2019-08-10 02:00:58 +0300

    trustzone: host application done

--- arm-trust-zone/include/hello_ta_uuid.h
+++ arm-trust-zone/include/hello_ta_uuid.h
@@ -1,6 +1,16 @@
 #ifndef _INCLUDED_HELLO_TA_UUID_H_
 #define _INCLUDED_HELLO_TA_UUID_H_
 
-#define HELLO_TA_UUID
+/*
+ * UUID generator at https://www.uuidgenerator.net/
+ */
+#define HELLO_TA_UUID \
+    { 0x4f5a5ab6, 0x0ecc, 0x4ad4, \
+       { 0x83, 0x7c, 0xd9, 0xfd, 0x7b, 0x70, 0x89, 0x78} }
+
+/*
+ * Define TA commands
+ */
+#define HELLO_TA_MULTIPLY_ARGS 0
 
 #endif  // _INCLUDED_HELLO_TA_UUID_H_
