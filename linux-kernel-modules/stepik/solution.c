28d3bf6ca1f90841af04278bbc36b225ad7f5869
Anisimov Alexander <anisimov.alexander.s@gmail.com>
2020-05-07 13:56:24 +0300

    stepik: kernel-objects: https://stepik.org/lesson/40444/step/2?unit=19141

--- linux-kernel-modules/stepik/solution.c
+++ linux-kernel-modules/stepik/solution.c
@@ -2,56 +2,81 @@
 #include <linux/types.h>
 #include <linux/fcntl.h>
 #include <linux/fs.h>
-#include <linux/slab.h> /* kmalloc() */
+#include <linux/slab.h>
 #include <linux/errno.h> 
 #include <linux/device.h> 
+#include <linux/kobject.h>
+#include <linux/string.h>
+#include <linux/sysfs.h>
+#include <linux/init.h>
 
-/* external module file */
-#include <checker.h>
-
-static void *p1 = NULL;
-static int *p2 = NULL;
-static struct device *p3 = NULL;
-
-/*
- * This is the starting point of the kernel module's code execution.
- * There you can allocate some memory, init global variables and so on.
- */
-static int solution_init(void)
+static ssize_t my_sys_show(struct kobject *kobj, struct kobj_attribute *attr,
+			   char *buf)
 {
-	/* allocate any pointer type */
-	p1 = kmalloc((size_t)get_void_size(), GFP_KERNEL);
-	submit_void_ptr(p1);
+	static int read_op_cnt = 0;
 
-	/* allocate array of ints */
-	p2 = (int *)kmalloc(sizeof(int) * get_int_array_size(), GFP_KERNEL);
-	submit_int_array_ptr(p2);
+	++read_op_cnt;
 
-	/* go mem for some device */
-	p3 = (struct device *)kmalloc(sizeof(struct device), GFP_KERNEL);
-	submit_struct_ptr(p3);
-
-	return 0;
+	return sprintf(buf, "%d\n", read_op_cnt);
 }
- 
-/*
- * This is the exit point of the kernel module. When the module is removed 
- * this function is called to do any sort of cleanup activities and other such
- * stuff.
- * 
- * For example you have made a tree where you keep some information - you would 
- * like to place the code for removing the nodes of the tree here.
- */
-static void solution_exit(void)
+
+static ssize_t my_sys_store(struct kobject *kobj, struct kobj_attribute *attr,
+			    const char *buf, size_t count)
 {
-	if (p1)
-		checker_kfree(p1);
+	return count;
+}
 
-	if (p2)
-		checker_kfree(p2);
+static struct kobj_attribute my_sys_attribute = 
+	__ATTR(my_sys, 0664, my_sys_show, my_sys_store);
 
-	if (p3)
-		checker_kfree(p3);
+/*
+ * Create a group of attributes so that we can create and destroy them all
+ * at once.
+ */
+static struct attribute *attrs[] = {
+	&my_sys_attribute.attr,
+	NULL, /* need to NULL terminate the list of attributes */
+};
+
+/*
+ * All this attributes will be expotred in your kobj dir.
+ *
+ * kobj will be created after kobject_create_and_add() call
+ * in sysfs
+ */
+static const struct attribute_group my_kobject_groups = {
+	.attrs = attrs,
+};
+
+static struct kobject *my_kobject;
+
+static int __init solution_init(void)
+{
+	int retval;
+
+	/*
+	 * This function creates a kobject structure dynamically and registers it
+	 * with sysfs with 'name'. When you are finished with this structure, call
+	 * kobject_put() and the structure will be dynamically freed when
+	 * it is no longer being used.
+	 *
+	 * For this kobj kernel uses default ktype with release function:
+	 * https://elixir.bootlin.com/linux/latest/source/lib/kobject.c#L750
+	 */
+	my_kobject = kobject_create_and_add("my_kobject", kernel_kobj);
+	if (!my_kobject)
+		return -ENOMEM;
+
+	retval = sysfs_create_group(my_kobject, &my_kobject_groups);
+	if (retval)
+		kobject_put(my_kobject);
+
+	return retval;
+}
+
+static void __exit solution_exit(void)
+{
+	kobject_put(my_kobject);	
 }
 
 module_init(solution_init);
@@ -59,4 +84,4 @@ module_exit(solution_exit);
 
 MODULE_DESCRIPTION("Learn Linux Kernel with stepik.org");
 MODULE_AUTHOR("Aleksandr Anisimov <anisimov.alexander.s@gmail.com>");
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL v2");
