22b941591ff85022ab7e11c3ac742b408100d916
Anisimov Alexander <anisimov.alexander.s@gmail.com>
2020-05-08 13:44:04 +0300

    stepik: dev-node: dynamic major num: https://stepik.org/lesson/40451/step/1?unit=19109

--- linux-kernel-modules/stepik/solution.c
+++ linux-kernel-modules/stepik/solution.c
@@ -13,24 +13,26 @@
 #include <linux/cdev.h>
 #include <linux/uaccess.h>
 
-static int open_cnt = 0;
-static int common_write_data_bytes = 0;
+#define MODULE_DEFNAME "solution_node1"
+#define MODULE_NAME_MAX_LEN 64
 
-static dev_t first; /* first number of device node */
-static unsigned int devcount = 1;
-static int my_major = 240; /* static major */
+static char in_node_name[MODULE_NAME_MAX_LEN];
+module_param_string(node_name, in_node_name, MODULE_NAME_MAX_LEN, 0664);
+
+static dev_t devno;
+static int my_major = 0;
 static int my_minor = 0;
 static struct cdev *devnode_cdev;
-
-#define MYDEV_NAME "solution_node"
+static struct class *devnode_class;
+static struct device *devnode_device;
 
 static ssize_t solution_read(struct file *file, char __user *buf, size_t lbuf,
 			     loff_t *ppos)
 {
+	static char show[20];
 	int showlen;
-	char show[50];
 
-	showlen = sprintf(show, "%d %d\n", open_cnt, common_write_data_bytes);
+	showlen = sprintf(show, "%d\n", my_major);
 
 	return simple_read_from_buffer(buf, lbuf, ppos, show, showlen);
 }
@@ -38,15 +40,11 @@ static ssize_t solution_read(struct file *file, char __user *buf, size_t lbuf,
 static ssize_t solution_write(struct file *file, const char __user *buf,
 			      size_t lbuf, loff_t *ppos)
 {
-	common_write_data_bytes += lbuf;
-
 	return lbuf;
 }
 
 static int solution_open(struct inode *inode, struct file *file)
 {
-	++open_cnt;
-
 	return 0;
 }
 
@@ -66,9 +64,19 @@ static struct file_operations fops = {
 static int __init solution_init(void)
 {
 	int retval = 0;
+	char *node_name;
 
-	first = MKDEV(my_major, my_minor);
-	register_chrdev_region(first, devcount, MYDEV_NAME);
+	if (!in_node_name[0])
+		node_name = MODULE_DEFNAME;
+	else
+		node_name = in_node_name;
+
+	retval = alloc_chrdev_region(&devno, my_minor, 1, node_name);
+	if (retval < 0)
+		return retval;
+
+	my_major = MAJOR(devno);
+	printk(KERN_INFO "dynamic major is %d\n", my_major);
 
 	/* allocate memory */
 	devnode_cdev = cdev_alloc();
@@ -78,25 +86,47 @@ static int __init solution_init(void)
 	/* assign fops to cdev */
 	cdev_init(devnode_cdev, &fops);
 
-	/*
-	 * Assign kernel object to common device table
-	 *
-	 * use sudo mknod /dev/solution_node c 240 0 2> /dev/null
-	 * to create node in devfs
-	 */
-	retval = cdev_add(devnode_cdev, first, devcount);
+	/* assign kernel object to common device table */
+	retval = cdev_add(devnode_cdev, devno, 1);
 	if (retval)
+		goto init_err;
+
+	devnode_class = class_create(THIS_MODULE, node_name);
+	if (!devnode_class)
+		goto init_err;
+
+	devnode_device = device_create(devnode_class, NULL, devno,
+			NULL, "%s", node_name);
+	if (!devnode_device)
+		goto init_err;
+
+	printk(KERN_INFO "device was created with name: %s\n", node_name);
+	retval = 0;
+	goto init_ok;
+
+init_err:
+	if (devnode_class)
+		class_destroy(devnode_class);
+
+	if (devnode_cdev)
 		cdev_del(devnode_cdev);
 
+init_ok:
 	return retval;
 }
 
 static void __exit solution_exit(void)
 {
+	if (devnode_device && devnode_class)
+		device_destroy(devnode_class, devno);
+
+	if (devnode_class)
+		class_destroy(devnode_class);
+
 	if (devnode_cdev)
 		cdev_del(devnode_cdev);
 
-	unregister_chrdev_region(first, devcount);
+	unregister_chrdev_region(devno, 1);
 }
 
 module_init(solution_init);
