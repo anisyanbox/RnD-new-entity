7570837742af5d23d23d7ef9f3ae3cd58979f1d8
Alexander Anisimov <anisimov.alexander.s@gmail.com>
2020-10-13 08:53:43 +0300

    allocator: initial

---
+++ allocators/main.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Hello, world!" << std::endl;
+	return EXIT_SUCCESS;
+}
+
