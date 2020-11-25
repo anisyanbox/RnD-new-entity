33f0465460c839937978578579a867344165d913
Alexander Anisimov <anisimov.alexander.s@gmail.com>
2020-11-25 03:49:19 +0300

    proc: find all children for known pid: https://stepik.org/lesson/27846/step/5?unit=9092

--- proc/solution.c
+++ proc/solution.c
@@ -32,8 +32,10 @@ static int check_input(int argc, char *argv[])
 	int i;
 	int len;
 	char *str;
-
-	if (argc == 2) {
+	
+	if (argc != 2) {
+		return 0;
+	} else {
 		str = argv[1];
 		len = strlen(str);
 
@@ -47,8 +49,8 @@ static int check_input(int argc, char *argv[])
 
 static int get_ppid(pid_t pid)
 {
-#define NEEDLE_STR	"PPid:\t"
-#define NEEDLE_STR_LEN	(sizeof(NEEDLE_STR) - 1)
+#define PPID_STR	"PPid:\t"
+#define PPID_STR_LEN	(sizeof(PPID_STR) - 1)
 
 	int i;
 	int n;
@@ -71,8 +73,6 @@ static int get_ppid(pid_t pid)
 		fprintf(stderr, "snpintf out of memory\n");
 		return -1;
 	}
-
-	debug_out("proc file name: %s\n", proc_fname);
 
 	fp = fopen(proc_fname, "r");
 	if (!fp) {
@@ -97,13 +97,13 @@ static int get_ppid(pid_t pid)
 			break_cond = true;
 		}
 
-		search_str = strstr((const char *)lineptr, NEEDLE_STR);
+		search_str = strstr((const char *)lineptr, PPID_STR);
 		if (!search_str)
 			continue;
 
 		/* replace EOL to '\0' */
 		for (i = 0; i < 32; ++i) {
-			ppid_str[i] = lineptr[NEEDLE_STR_LEN + i];
+			ppid_str[i] = lineptr[PPID_STR_LEN + i];
 			if (ppid_str[i] == '\n') {
 				ppid_str[i] = '\0';
 				break_cond = true;
@@ -119,9 +119,76 @@ static int get_ppid(pid_t pid)
 	return atoi(ppid_str);
 }
 
+static int proc_dirs_selector(const struct dirent *d)
+{
+	int i;
+	int c;
+
+	for (i = 0; i < 256; ++i) {
+		c = d->d_name[i];
+
+		if (c == '\0')
+			break;
+
+		if (!isdigit(c))
+			return 0;
+	}
+
+	return 1;
+}
+
+static int get_proc_dirs(struct dirent ***namelist)
+{
+	int n;
+
+	n = scandir("/proc", namelist, proc_dirs_selector, alphasort);
+	if (n == -1) {
+		fprintf(stderr, "scandir() failed: %s\n", strerror(errno));
+		*namelist = NULL;
+	}
+
+	return n;
+}
+
+/*
+ * Recursice function to find all child processes for known pid.
+ *
+ * @pid		- parent pid
+ * @proc_list	- list of all available processes's dirs in system
+ * @proc_cnt	- count of items in proclist
+ *
+ * Returns count of children.
+ */
+static int get_all_child_for_pid(pid_t pid, struct dirent **proc_list,
+	int proc_cnt)
+{
+	int i, n;
+	int children = 0;
+
+	pid_t ppid;
+	pid_t temp_pid;
+
+	for (i = 0; i < proc_cnt; ++i) {
+		temp_pid = atoi((*proc_list[i]).d_name);
+		ppid = get_ppid(temp_pid);
+
+		/* ppid for some proc == input PID --> it means
+		 that this */
+		if (ppid == pid) {
+			++children;
+			children += get_all_child_for_pid(temp_pid,
+				proc_list, proc_cnt);
+		}
+	}
+
+	return children;
+}
+
 int main(int argc, char *argv[])
 {
 	pid_t pid;
+	int i, n;
+	struct dirent **namelist;
 
 	pid = (pid_t)check_input(argc, argv);
 	if (pid == 0) {
@@ -129,17 +196,17 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	printf("%d\n", pid);
-	while (true) {
-		pid = get_ppid(pid);
-		if (pid <= 0)
-			return EXIT_FAILURE;
-
-		printf("%d\n", pid);
-
-		if (pid == 1)
-			break;
+	n = get_proc_dirs(&namelist);
+	if (!namelist) {
+		fprintf(stderr, "get right proc dirs failed\n");
+		return EXIT_FAILURE;
 	}
+
+	printf("%d\n", get_all_child_for_pid(pid, namelist, n) + 1);
+
+	while (n--)
+		free(namelist[n]);
+	free(namelist);
 
 	return EXIT_SUCCESS;
 }
